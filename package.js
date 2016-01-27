67adb609ccb6cef1945ab1c4fa37ed454c300533
tkay <aruntknambiar@gmail.com>
2016-01-27 22:53:03 +0530

    bump up versions

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mwc:mixin',
-  version: '1.0.2',
+  version: '1.0.3',
   summary: 'Reactive Data Mixin For Polymer',
     git: "https://github.com/meteorwebcomponents/mixin.git",
   documentation: "README.md"
