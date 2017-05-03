1b18a9d47d29f3813d3b55b7a0b58f8189944ad1
aruntk <aruntknambiar@gmail.com>
2017-05-03 17:22:04 +0530

    versions

--- dist/mwc-mixin.js
+++ dist/mwc-mixin.js
@@ -1,10 +1,5 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.MwcMixin = undefined;
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
@@ -44,7 +39,7 @@ var mwcDataUpdate = function mwcDataUpdate(element) {
   });
 };
 
-var MwcMixin = exports.MwcMixin = function MwcMixin(parent) {
+MwcMixin = function MwcMixin(parent) {
   return function (_parent) {
     _inherits(_class, _parent);
 
