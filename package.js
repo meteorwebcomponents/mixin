30d71745a77dea36b1ba039631a8701becf05084
aruntk <aruntknambiar@gmail.com>
2017-05-03 16:17:21 +0530

    2.0.0-beta.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mwc:mixin',
-  version: '2.0.0-beta',
+  version: "2.0.0-beta.1",
   summary: 'Reactive Data Mixin For Polymer',
   git: 'https://github.com/meteorwebcomponents/mixin.git',
   documentation: 'README.md',
